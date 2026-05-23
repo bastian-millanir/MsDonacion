19acc94b3aa05ef1f1999d9ca8eb1577ebb45602
bastian-millanir <bastian.millanir@gmail.com>
2026-05-22 20:44:19 -0400

    feat: add REMOVE notifications based on ID

--- MsNotificaciones/src/main/java/com/donaton/msnotificaciones/controller/NotificacionController.java
+++ MsNotificaciones/src/main/java/com/donaton/msnotificaciones/controller/NotificacionController.java
@@ -1,9 +1,13 @@
 package com.donaton.msnotificaciones.controller;
 
+import com.donaton.msnotificaciones.model.Notificacion;
 import com.donaton.msnotificaciones.service.NotificacionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +22,13 @@ public class NotificacionController {
     private final NotificacionService notificacionService;
 
     @GetMapping
-    public List<String> obtener() {
+    public List<Notificacion> obtener() {
         return notificacionService.obtenerNotificaciones();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable String id) {
+        notificacionService.eliminarNotificacion(id);
+        return ResponseEntity.noContent().build();
     }
 }
